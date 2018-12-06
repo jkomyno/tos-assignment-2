7fb100820f9ba92389ef3f5de457239682cd0380
Alberto Schiabel <skiabo97@gmail.com>
2018-12-06 22:44:53 +0100

    Added comments to getOrderPrice(). Added first trivial assertion

--- src/main/java/it/unipd/tos/business/RestaurantBillImpl.java
+++ src/main/java/it/unipd/tos/business/RestaurantBillImpl.java
@@ -10,8 +10,26 @@ import it.unipd.tos.model.MenuItem;
 import java.util.List;
 
 public class RestaurantBillImpl implements RestaurantBill {
+
     @Override
+    /**
+     * Dato un elenco di ordinazioni (Pizze e Primi piatti) calcolare il totale (somma del prezzo
+     * dei prodotti ordinati)
+     *
+     * Se vengono ordinate più di 10 Pizze la meno costosa è in regalo (l’importo totale non
+     * considera la pizza meno costosa)
+     *
+     * Se l’importo totale delle ordinazioni (Pizze e Primi) supera i 100 euro viene fatto un 5%
+     * di sconto
+     *
+     * Non è possibile avere un’ordinazione con più di 20 elementi (se accade prevedere un
+     * messaggio d’errore
+     *
+     * @param itemsOrdered
+     * @return
+     * @throws RestaurantBillException
+     */
     public double getOrderPrice(List<MenuItem> itemsOrdered) throws RestaurantBillException {
-        return -1;
+        return 0;
     }
 }
