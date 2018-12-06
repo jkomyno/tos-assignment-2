549ff7b29e9befc1d449c010ce53a0f771c8f86e
Alberto Schiabel <skiabo97@gmail.com>
2018-12-06 22:44:53 +0100

    Added comments to getOrderPrice(). Added first trivial assertion

--- src/test/java/it/unipd/tos/business/RestaurantBillImplTest.java
+++ src/test/java/it/unipd/tos/business/RestaurantBillImplTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class RestaurantBillImplTest {
+
     @Test
     public void test() {
         RestaurantBillImpl bill = new RestaurantBillImpl();
